794cb2ee2fb08ffcad7b1ca241c86542ae92670a
DebasishKisan1 <debasish.kisan@gmail.com>
2024-01-21 11:54:48 +0530

    localstorage added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import './App.css';
 import OrderForm from './Components/OrderForm';
 import OrderDetails from './Components/OrderDetails';
 import Orderctx from './store/Orderctx';
-import {useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [orders, setOrders] = useState([])
+  useEffect(()=>{
+    if (localStorage.getItem('restaurantOrders')) {
+      setOrders(()=>JSON.parse(localStorage.getItem('restaurantOrders')))
+    }
+  },[])
+  useEffect(() => {
+    if (orders.length > 0) {
+      localStorage.setItem('restaurantOrders', JSON.stringify(orders))
+    }
+  }, [orders])
   return (
     <Orderctx.Provider value={{ orders: orders, setOrders: setOrders }}>
       <OrderForm />
-      <OrderDetails />
+      {orders.length > 0 && <OrderDetails />}
     </Orderctx.Provider>
   );
 }
